1e82bde7bcd1b2864e44bec67755df45c8a7acc9
DIVYAPRABHA1602 <divyaprabha1602@gmail.com>
2016-07-25 22:46:25 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+int i;
+char ch[10]="reverseme",ch2[10]=msrv;
+puts(ch);
+puts(ch2);
+cch[10]=strrev(ch);
+cch2[]=strrev(ch2);
+for(i=0;ch[i]!="0";i++)
+{
+if(ch[i]=="a-z" && ch1[i]=="a-z")
+{
+cch[i] ==cch[i+1])
+cch2[i]=cch2[i+1);
+}
+puts(cch);
+puts(cch2);
+}
+
+
